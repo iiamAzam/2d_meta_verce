327ea1c024d4835bfe7d7716df0bb9cb7cce93eb
artist0pa <azambari2001@gmail.com>
2024-10-30 21:30:04 +0530

    brginning of auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Adjust if React runs on a different port
+    origin: "http://localhost:5173", 
     methods: ["GET", "POST"]
   }
 });
