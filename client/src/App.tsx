bae3afafdc7332891639548c7dec89d709d32ed4
artist0pa <azambari2001@gmail.com>
2024-11-12 19:54:59 +0530

    adding gradiant bg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,48 @@
-import  './App.css';
-
+import React from 'react';
 
 function App() {
-
   return (
-    <div className='bg-black'>
-      <h1>ok</h1>
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-red-500 via-purple-500 to-blue-500 animate-gradient bg-[length:200%_200%]">
+      <div className="text-white text-center">
+        <h1 className="text-5xl font-bold">Pixel Streaming</h1>
+        <p className="mt-4 text-xl">Delivering solutions at lightning speed!</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import  './App.css';
+
+
+// function App() {
+
+//   return (
+//     <div className='bg-black'>
+//       <h1>ok</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
